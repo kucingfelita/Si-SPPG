a05df582552b216baf62df5e3467ed38eee5f35b
ethan <hariansyahegie@gmail.com>
2025-11-02 13:42:51 +0700

    edit footer

--- template/footer.php
+++ template/footer.php
@@ -1,4 +1,17 @@
-<!-- template/footer.php -->
-<div class="footer" style="text-align:center;color:#000;font-size:13px;margin-top:40px;padding:10px 0;">
-    ©2025 Satuan Pelayanan Pemenuhan Gizi. All rights reserved.
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>footer</title>
+   <link rel="stylesheet" href="../assets/css/footer.css">
+</head>
+
+<body>
+   <footer class="site-footer">
+      <p class="footer-p">&copy;2025 Satuan Pelayanan Pemenuhan Gizi. All rights reserved</p>
+   </footer>
+</body>
+
+</html>

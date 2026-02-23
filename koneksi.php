960a109ff88a2519141655362636fe1eb4d6c1c1
ethan <hariansyahegie@gmail.com>
2026-02-23 10:53:32 +0700

    koneksi edit

---
+++ koneksi.php
@@ -0,0 +1,21 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "120408";
+$db = "db_si_sppg";
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+function check_and_reconnect($koneksi) {
+    if (!mysqli_ping($koneksi)) {
+        $koneksi = mysqli_connect($GLOBALS['host'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['db']);
+        if (!$koneksi) {
+            die("Koneksi gagal: " . mysqli_connect_error());
+        }
+    }
+    return $koneksi;
+}
+?>
